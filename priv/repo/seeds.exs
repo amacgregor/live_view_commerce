4d7b2cda6dbe4181f7135ab75d63904912784987
Allan MacGregor <amacgregor@allanmacgregor.com>
2019-10-06 12:53:10 -0400

    feat: seeder file for generating sample products

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,9 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias LiveViewCommerce.Catalog
+alias Faker.{Commerce, Lorem}
+
+skus = Faker.Util.sample_uniq(10, fn -> Faker.String.base64(8) end)
+
+Enum.each(skus, fn(sku) -> Catalog.create_product(%{sku: sku, name: Commerce.product_name(), price: Commerce.price, description: Lorem.sentence(5..15)}) end)
